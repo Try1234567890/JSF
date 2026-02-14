b2ebc1233e47216feedd351f30bdf2ee9ee23b9a
Try1234567890 <paolo.guccionemz@gmail.com>
2026-02-14 21:47:58 +0100

    fix: re-organized the class

--- main/src/main/java/me/tr/trformatter/TrFormatterAPI.java
+++ main/src/main/java/me/tr/trformatter/TrFormatterAPI.java
@@ -3,6 +3,7 @@ package me.tr.trformatter;
 import me.tr.trformatter.phases.analysis.lexer.defaults.PlaceholderLexer;
 import me.tr.trformatter.phases.analysis.lexer.tokens.components.PlaceholderToken;
 import me.tr.trformatter.phases.analysis.parser.defaults.PlaceholderParser;
+import me.tr.trformatter.phases.analysis.scanner.chars.CharacterSet;
 import me.tr.trformatter.phases.analysis.scanner.components.IndexedRawPlaceholder;
 import me.tr.trformatter.phases.analysis.scanner.defaults.PlaceholderScanner;
 import me.tr.trformatter.phases.evaluation.components.EvalPlaceholder;
@@ -13,22 +14,24 @@ import me.tr.trformatter.phases.synthesis.interpolator.components.IndexedPlaceho
 import java.util.List;
 
 public class TrFormatterAPI {
-    private static final PlaceholderScanner SCANNER = new PlaceholderScanner();
-    private static final PlaceholderLexer TOKENIZER = new PlaceholderLexer();
-    private static final PlaceholderParser PARSER = new PlaceholderParser();
-    private static final PlaceholderEvaluator EVALUATOR = new PlaceholderEvaluator();
-    private static final PlaceholderInterpolator INTERPOLATOR = new PlaceholderInterpolator();
 
     private TrFormatterAPI() {
-
     }
 
     public static String format(String text) {
-        List<IndexedRawPlaceholder> raws = SCANNER.scan(text);
-        List<PlaceholderToken> tokens = TOKENIZER.tokenize(raws);
-        List<EvalPlaceholder> evaluators = PARSER.parse(tokens);
-        List<IndexedPlaceholder> placeholders = EVALUATOR.evaluate(evaluators);
+        return format(text, CharacterSet.DEFAULT);
+    }
 
-        return INTERPOLATOR.interpolate(text, placeholders);
+    public static String format(String text, CharacterSet characters) {
+        List<IndexedRawPlaceholder> raws = PlaceholderScanner.of(characters).scan(text);
+        if (raws.isEmpty()) {
+            // Simple optimization, do not continue if there is no placeholders.
+            return text;
+        }
+
+        List<PlaceholderToken> tokens = PlaceholderLexer.of(characters).tokenize(raws);
+        List<EvalPlaceholder> evaluators = PlaceholderParser.INSTANCE.parseAll(tokens);
+        List<IndexedPlaceholder> placeholders = PlaceholderEvaluator.INSTANCE.evaluate(evaluators);
+        return PlaceholderInterpolator.INSTANCE.interpolate(text, placeholders);
     }
 }
